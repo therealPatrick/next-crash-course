d53fe040cb7458b15439fe6894bdac5d4926bdbf
therealPatrick <malamashangala13@gmail.com>
2023-07-18 19:06:20 +0200

    initial

---
+++ app/about/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About my website</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nesciunt, magnam eveniet expedita et nisi ipsam illo.
+                Dolores et quibusdam voluptatem?
+            </p>
+        </div>
+    )
+}
+
+export default AboutPage
---
+++ app/about/team/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TeamPage = () => {
+    return (
+        <div>
+            <h1>Our Team</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eveniet temporibus, suscipit rem aliquam vitae mollitia, reiciendis id nam amet magnam
+                ut, porro possimus dicta fuga omnis deleniti. Odio, ut.
+            </p>
+        </div>
+    )
+}
+
+export default TeamPage
